0424636f258b6ec541a3c4db8304ad01e05f0513
ragulv-cd <76117375+ragul666@users.noreply.github.com>
2022-10-30 12:15:08 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect, useRef } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Login from '../src/pages/Login'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
